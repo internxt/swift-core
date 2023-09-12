62a81b0fe7ca2d90cc6ff7c9de0d36ead92ebe71
Pixo <robertogarciadevelopment@gmail.com>
2023-09-12 12:50:26 +0200

    Add status to folder response

--- Sources/InternxtSwiftCore/Types/DriveTypes.swift
+++ Sources/InternxtSwiftCore/Types/DriveTypes.swift
@@ -56,6 +56,7 @@ public struct GetFolderFoldersResult: Decodable {
     public let removed: Bool?
     // ISO string
     public let removedAt: String?
+    public let status: String
 }
 
 public struct GetFolderFoldersResponse: Decodable {
